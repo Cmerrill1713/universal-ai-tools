262505bb74e33e5efdd6254fbf45ed8b7fb02758
Cmerrill1713 <cmerrill1713@gmail.com>
2025-07-28 20:37:27 -0500

    test: GitHub automation demo file

---
+++ test-automation.js
@@ -0,0 +1,16 @@
+// Test file to demonstrate GitHub automation
+// This file has intentional formatting issues that should be auto-fixed
+
+const test = {
+  name: "GitHub Automation Test",
+  badFormatting: true,
+  inconsistentSpacing: "everywhere"
+};
+
+console.log("This will be auto-formatted by Prettier");
+
+if (test.badFormatting) {
+  console.log("ESLint will fix this spacing");
+}
+
+export default test;
